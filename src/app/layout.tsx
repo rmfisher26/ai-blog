cd21aa47e1221467963a0ccede0d240d91b5616b
Robert Fisher <fisher.robert26@gmail.com>
2025-10-14 16:57:11 -0500

    Day 25 — Add Analytics (Plausible / Vercel Analytics)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from 'next-auth/react'
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider>{children}</SessionProvider>
+        <Analytics />
+
       </body>
     </html>
   );
